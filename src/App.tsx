e73d1fcdbd925181b6ef8484281c8b28710ddc18
Mehrob007 <mehrobsanginov2006@gmail.com>
2025-10-16 14:57:43 +0500

    refaktaring

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@ import { useEffect } from "react";
 export default function App() {
   const platform = navigator.platform;
   useEffect(() => {
-    if (platform.startsWith("Win")) {
-      window.location.href =
-        "https://play.google.com/store/apps/details?id=tj.edonish";
-    } else if (platform.startsWith("Mac")) {
-      window.location.href =
-        "https://apps.apple.com/tj/app/edonish/id1579282546";
-    } else if (platform.startsWith("iPhone") || platform.startsWith("iPad")) {
+    if (
+      platform.startsWith("iPhone") ||
+      platform.startsWith("iPad") ||
+      platform.startsWith("Mac")
+    ) {
       window.location.href =
         "https://apps.apple.com/tj/app/edonish/id1579282546";
     } else {
