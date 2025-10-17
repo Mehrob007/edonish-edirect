44fbe34cb7c4470c50ece6b8fb97f23367a414d2
Mehrob007 <mehrobsanginov2006@gmail.com>
2025-10-17 13:04:00 +0500

    edit: styles and size

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,19 @@ import mobile from "./assets/mobi.png";
 export default function App() {
   const platform = navigator.platform;
   useEffect(() => {
-    if (
-      platform.startsWith("iPhone") ||
-      platform.startsWith("iPad") ||
-      platform.startsWith("Mac")
-    ) {
-      window.location.href =
-        "https://apps.apple.com/tj/app/edonish/id1579282546";
-    } else {
-      window.location.href =
-        "https://play.google.com/store/apps/details?id=tj.edonish";
-    }
+    setTimeout(() => {
+      if (
+        platform.startsWith("iPhone") ||
+        platform.startsWith("iPad") ||
+        platform.startsWith("Mac")
+      ) {
+        window.location.href =
+          "https://apps.apple.com/tj/app/edonish/id1579282546";
+      } else {
+        window.location.href =
+          "https://play.google.com/store/apps/details?id=tj.edonish";
+      }
+    }, 1000);
   }, [platform]);
   return (
     <main>
